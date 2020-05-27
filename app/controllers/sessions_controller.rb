abea51224177c72528c169f1d88333b9d746a0fe
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-27 16:32:16 -0600

    Logged in users will go to their dashboard when trying to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,19 @@
 class SessionsController < ApplicationController
-    def new
+  def new
+
+    if session[:user_id]
+      user = User.find_by(id: session[:user_id])
+      flash[:notice] = "You are already logged in"
+
+      if user.role == "admin"
+        redirect_to "/admin/dashboard"
+      elsif user.role == "merchant"
+        redirect_to "/merchant/dashboard"
+      else
+        redirect_to "/profile"
+      end
     end
+  end
 
     def create
 
--- spec/features/login/login_spec.rb
+++ spec/features/login/login_spec.rb
@@ -118,15 +118,67 @@ RSpec.describe "Register Index Page", type: :feature do
       email: "jane@hotmail.com",
       password: "3455",
       password_confirmation: "3455",
-      role: 2
+      role: 0
       })
 
     visit "/login"
     fill_in :email, with: "jane@hotmail.com"
-    fill_in :password, with: "2222"
+    fill_in :password, with: "3455"
     click_on "Submit"
-
+    expect(current_path).to eql("/profile")
+    visit "/login"
+    expect(current_path).to eql("/profile")
+    expect(page).to have_content("You are already logged in")
   end
 
+  it "redirects merchant users that are already logged in" do
 
+    jil = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "adsc@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 1
+      })
+      visit "/login"
+      fill_in :email, with: "adsc@hotmail.com"
+      fill_in :password, with: "3455"
+      click_on "Submit"
+      expect(current_path).to eql("/merchant/dashboard")
+      visit "/login"
+      expect(current_path).to eql("/merchant/dashboard")
+      expect(page).to have_content("You are already logged in")
+    end
+
+  it "redirects admin users that are already logged in" do
+    jil = User.create!({
+    name: "Jack",
+    address: "333 Jack Blvd",
+    city: "Denver",
+    state: "Colorado",
+    zip: 83243,
+    email: "ssss@hotmail.com",
+    password: "3455",
+    password_confirmation: "3455",
+    role: 2
+    })
+    visit "/login"
+    fill_in :email, with: "ssss@hotmail.com"
+    fill_in :password, with: "3455"
+    click_on "Submit"
+    expect(current_path).to eql("/admin/dashboard")
+    visit "/login"
+    expect(current_path).to eql("/admin/dashboard")
+    expect(page).to have_content("You are already logged in")
+  end
 end
+# As a registered user, merchant, or admin
+# When I visit the login path
+# If I am a regular user, I am redirected to my profile page
+# If I am a merchant user, I am redirected to my merchant dashboard page
+# If I am an admin user, I am redirected to my admin dashboard page
+# And I see a flash message that tells me I am already logged in
