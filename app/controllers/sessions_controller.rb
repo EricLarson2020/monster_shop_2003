094affb0227896aae7c5f2232317743175336acb
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-27 16:03:41 -0600

    Created test and method if invalid credentials are entered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,30 @@
 class SessionsController < ApplicationController
-    def new 
-    end 
+    def new
+    end
 
-    def create 
-        user = User.find_by(params[:id])
+    def create
+
+      if User.find_by(email: params[:email])
+        user = User.find_by(email: params[:email])
         # binding.pry
         session[:user_id] = user.id
-        if user.role == "admin" 
+        if user.authenticate(params[:password]) && user.role == "admin"
             flash[:notice] = "You are logged in as #{user.name}"
             redirect_to "/admin/dashboard"
-        elsif user.role == "merchant"
+        elsif user.authenticate(params[:password]) && user.role == "merchant"
             flash[:notice] = "You are logged in as #{user.name}"
             redirect_to "/merchant/dashboard"
-        else 
+        elsif user.authenticate(params[:password]) && user.role == "default"
             flash[:notice] = "You are logged in as #{user.name}"
             redirect_to "/profile"
-        end 
-    end 
-end 
+        else
+            flash[:error] = "Sorry, your credentials are bad."
+            render :new
+
+        end
+      else
+        flash[:error] = "Sorry, your credentials are bad."
+        render :new
+      end
+    end
+end
--- spec/features/login/login_spec.rb
+++ spec/features/login/login_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Register Index Page", type: :feature do
-  it "basic user can log in" do 
+  it "basic user can log in" do
     visit "/login"
-        
+
    jack = User.create!({
       name: "Jack",
       address: "333 Jack Blvd",
@@ -11,60 +11,122 @@ RSpec.describe "Register Index Page", type: :feature do
       state: "Colorado",
       zip: 83243,
       email: "jack@hotmail.com",
-      password: "3455", 
+      password: "3455",
       password_confirmation: "3455",
       role: 0
       })
-      
+
     fill_in :email, with: "jack@hotmail.com"
     fill_in :password, with: "3455"
     click_on "Submit"
     expect(current_path).to eq("/profile")
-    expect(page).to have_content("You are logged in as Jack")   
-    end 
+    expect(page).to have_content("You are logged in as Jack")
+  end
 
-    it "merchant user can login" do 
-      jack = User.create!({
-        name: "Jack",
-        address: "333 Jack Blvd",
-        city: "Denver",
-        state: "Colorado",
-        zip: 83243,
-        email: "john@hotmail.com",
-        password: "3455", 
-        password_confirmation: "3455",
-        role: 1
-        })
+  it "merchant user can login" do
+    jack = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "john@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 1
+      })
 
-        visit "/login"
-        fill_in :email, with: "john@hotmail.com"
-        fill_in :password, with: "3455"
-        click_on "Submit"
-        expect(current_path).to eq("/merchant/dashboard")
-        expect(page).to have_content("You are logged in as Jack")   
-   
-    end 
+      visit "/login"
+      fill_in :email, with: "john@hotmail.com"
+      fill_in :password, with: "3455"
+      click_on "Submit"
+      expect(current_path).to eq("/merchant/dashboard")
+      expect(page).to have_content("You are logged in as Jack")
 
-    it "admin user can login" do 
-      jack = User.create!({
-        name: "Jack",
-        address: "333 Jack Blvd",
-        city: "Denver",
-        state: "Colorado",
-        zip: 83243,
-        email: "jake@hotmail.com",
-        password: "3455", 
-        password_confirmation: "3455",
-        role: 2
-        })
+  end
 
-        visit "/login"
-        fill_in :email, with: "jake@hotmail.com"
-        fill_in :password, with: "3455"
-        click_on "Submit"
-        expect(current_path).to eq("/admin/dashboard") 
-        expect(page).to have_content("You are logged in as Jack")     
-    end 
+  it "admin user can login" do
+    jack = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "jake@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 2
+      })
+
+      visit "/login"
+      fill_in :email, with: "jake@hotmail.com"
+      fill_in :password, with: "3455"
+      click_on "Submit"
+      expect(current_path).to eq("/admin/dashboard")
+      expect(page).to have_content("You are logged in as Jack")
+  end
+
+  it "can not log in with invalid email" do
+    jil = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "jil@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 2
+      })
+
+    visit "/login"
+    fill_in :email, with: "bob@hotmail.com"
+    fill_in :password, with: "3455"
+    click_on "Submit"
+    expect(current_path).to eql("/login")
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+
+  it "can not log in with invalid email" do
+    jil = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "jane@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 2
+      })
+
+    visit "/login"
+    fill_in :email, with: "jane@hotmail.com"
+    fill_in :password, with: "2222"
+    click_on "Submit"
+    expect(current_path).to eql("/login")
+    expect(page).to have_content("Sorry, your credentials are bad.")
+  end
+
+  it "redirects default user that are already logged in" do
+    jil = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "jane@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 2
+      })
+
+    visit "/login"
+    fill_in :email, with: "jane@hotmail.com"
+    fill_in :password, with: "2222"
+    click_on "Submit"
+
+  end
 
 
-end 
+end
