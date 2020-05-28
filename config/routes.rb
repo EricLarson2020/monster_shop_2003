5f9461eaf065becae60f72dd7d7b0ddf8112cf38
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-27 19:28:46 -0600

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: 'welcome#index'
+  get "/", to: "merchants#index"
 
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
