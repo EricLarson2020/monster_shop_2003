25535465351fe68fcaccc7166211b11c10f21f69
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-27 19:32:07 -0600

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
+require "BCrypt"
 class User < ApplicationRecord
+
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
   validates_presence_of :name, :address, :city, :state, :zip
