fd48766fa268bc48ba43d50cea57142efa6701ca
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-27 18:38:45 -0600

    Created logout and profile page links

--- spec/features/login/logout_spec.rb
+++ spec/features/login/logout_spec.rb
@@ -46,4 +46,35 @@ RSpec.describe "Register Index Page", type: :feature do
   expect(page).to_not have_css("#cart-item-#{@pencil.id}")
   expect(page).to_not have_css("#cart-item-#{@paper.id}")
   end
-end
+
+  it "can see and click logout in navigation bar if logged in" do
+    jack = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "sss",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 2
+      })
+
+      visit "/login"
+      fill_in :email, with: "sss"
+      fill_in :password, with: "3455"
+      click_on "Submit"
+      visit "/"
+      within ".topnav" do
+        click_link("logout")
+      end
+      expect(current_path).to eql("/")
+    end
+
+    it "does not see logout button if not logged in" do
+    visit "/items"
+    within ".topnav" do
+      expect(page).not_to have_css("logout")
+    end
+  end
+  end
