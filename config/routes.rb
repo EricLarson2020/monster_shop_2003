c0914c696e4f6dc71d48d42d61eeb3b3ebe038ce
Jack Gould <johnhilsmangould@gmail.com>
2020-05-31 12:24:40 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   get "/cart", to: "cart#show"
   delete "/cart", to: "cart#empty"
   delete "/cart/:item_id", to: "cart#remove_item"
+  patch "/cart/:item_id/update", to: "cart#update"
 
   get "/orders/new", to: "orders#new"
   post "/orders", to: "orders#create"
