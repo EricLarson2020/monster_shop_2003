f79effdd7963a12ad318f45cb077f7db8d50d6c2
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-31 07:18:33 -0600

    Created user_story_27 test

--- spec/features/profile/index_spec.rb
+++ spec/features/profile/index_spec.rb
@@ -44,10 +44,45 @@ RSpec.describe "Profile Index Page", type: :feature do
       role: 0
       })
 
+
+
       visit "/login"
       fill_in :email, with: "puppy@hotmail.com"
       fill_in :password, with: "3455"
       click_on "Submit"
+
+      bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = bike_shop.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033, user_id: jack.id)
+      order_1.item_orders.create!(item: chain, price: chain.price, quantity: 2)
+
+      visit "/profile"
+      click_link "My Orders"
+      expect(current_path).to eql("/profile/orders")
+
+  end
+
+  it "does not have link if no orders are present" do
+    jack = User.create!({
+      name: "Jack",
+      address: "333 Jack Blvd",
+      city: "Denver",
+      state: "Colorado",
+      zip: 83243,
+      email: "puppy@hotmail.com",
+      password: "3455",
+      password_confirmation: "3455",
+      role: 0
+      })
+
+
+
+      visit "/login"
+      fill_in :email, with: "puppy@hotmail.com"
+      fill_in :password, with: "3455"
+      click_on "Submit"
+      visit "/profile"
+      expect(page).not_to have_link("My Orders")
 
   end
 end
