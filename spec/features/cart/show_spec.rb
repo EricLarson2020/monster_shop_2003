2609386a8256fe35db44bedb67039c9406209ce4
Jack Gould <johnhilsmangould@gmail.com>
2020-05-31 11:07:41 -0600

    add test for increaseing quantity

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -70,6 +70,22 @@ RSpec.describe 'Cart show' do
         expect(page).to_not have_link("Empty Cart")
       end
 
+      it "user can increase/decrease quantity from cart" do
+        mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+        meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+
+        tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+        pencil = mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+
+        visit "/items/#{paper.id}"
+        click_on "Add To Cart"
+        visit '/cart'
+        fill_in 'quantity', with: 5
+
+        expect(page).to have_content("Total: $100")
+      end
+
     end
   end
 end
