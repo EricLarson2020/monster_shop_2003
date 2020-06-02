abbb58188f9e2be3def9b0195aea31ec57cfbf3b
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-02 09:24:24 -0600

    add prepopulated fields for new form when redirected with incorrect info

--- app/controllers/merchant/items_controller.rb
+++ app/controllers/merchant/items_controller.rb
@@ -4,6 +4,7 @@ class  Merchant::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
--- spec/features/merchants/items/new_spec.rb
+++ spec/features/merchants/items/new_spec.rb
@@ -73,23 +73,6 @@ RSpec.describe "As a merchant employee, when I visit my items page" do
 
     click_on "Create Item"
     expect(current_path).to eq("/merchant/items")
-    # expect(page).to have_content("Name can't be blank")
-    # how to search page to find css
+    expect(page).to have_css("img[src*='https://cdn.dribbble.com/users/120891/screenshots/4649285/dribble_42.png']")
   end
 end
-
-
-# I see a link to add a new item
-# When I click on the link to add a new item
-# I see a form where I can add new information about an item, including:
-# - the name of the item, which cannot be blank
-# - a description for the item, which cannot be blank
-# - a thumbnail image URL string, which CAN be left blank
-# - a price which must be greater than $0.00
-# - my current inventory count of this item which is 0 or greater
-
-# When I submit valid information and submit the form
-# I am taken back to my items page
-# I see a flash message indicating my new item is saved
-# I see the new item on the page, and it is enabled and available for sale
-# If I left the image field blank, I see a placeholder image for the thumbnail
