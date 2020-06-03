a247f71f34545f43452f63ead65fd6d4351822bc
Whitney Kidd <whitneymkidd@gmail.com>
2020-06-03 14:15:32 -0600

    complete story 53

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,17 @@
+class Admin::UsersController < ApplicationController
+  before_action :require_admin
+
+  # def show
+  #   @user = User.find(params[:id])
+  # end
+
+  def index
+    @users = User.all
+  end
+
+  private
+
+  def require_admin
+    render file: "/public/404" unless current_admin?
+  end
+end
